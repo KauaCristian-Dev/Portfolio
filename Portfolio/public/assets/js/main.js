1f641e283a491ebe87a0096114fb17f9d0ab9971
Kaka <dev.kaua.tech@gmail.com>
2026-09-25 23:27:19 -0300

    feat(skills): faz seçao de skill. nao é o final

--- Portfolio/public/assets/js/main.js
+++ Portfolio/public/assets/js/main.js
@@ -1,16 +1,37 @@
 const bgVideo = document.getElementById('bg-contact');
 let reverseInternal;
 
-bgVideo.addEventListener('ended', function(){
+if (bgVideo) {
+  bgVideo.addEventListener('ended', function(){
     bgVideo.pause();
 
     reverseInternal = setInterval(() => {
-        if(bgVideo.currentTime > 0){
-            bgVideo.currentTime -= 0.4;
-        } else {
-            clearInterval(reverseInternal);
-            bgVideo.play();
-        }
+      if(bgVideo.currentTime > 0){
+        bgVideo.currentTime -= 0.4;
+      } else {
+        clearInterval(reverseInternal);
+        bgVideo.play();
+      }
     }, 30)
+  });
+}
+
+// ======= header ========== //
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
 });
 
+// ========== link ========
+
+const card = document.getElementById('github-link');
+
+card.addEventListener('click', () => {
+    window.open('https://github.com/KauaCristian-Dev', '_blank');
+});
